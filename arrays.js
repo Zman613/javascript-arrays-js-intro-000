e6ebe5642521106a65ef738268e6e21e2967ac0d
Chaim Muskal <monthlyschnitzel@gmail.com>
2019-08-09 22:19:06 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -5,29 +5,29 @@ function addElementToBeginningOfArray(array, addition) {
 }
 function destructivelyAddElementToBeginningOfArray(array, addition){
   array.unshift(addition)
-  return [array]
+  return array
 }
 function addElementToEndOfArray(array, addition){
   return [...array, addition]
 }
 function destructivelyAddElementToEndOfArray(array, addition){
   array.shift(addition)
-  return [array]
+  return array
 }
 function accessElementInArray(array, index){
   return [array[index]]
 }
 function destructivelyRemoveElementFromBeginningOfArray(array){
   array.shift()
-  return[array]
+  return array
 }
 function removeElementFromBeginningOfArray(array){
-  return [array.slice(1)]
+  return array.slice(1)
 }
 function destructivelyRemoveElementFromEndOfArray(array){
   array.pop()
-  return [array]
+  return array
 }
 function removeElementFromEndOfArray(array){
-  return [array.slice(0, array.length -1)]
+  return array.slice(0, array.length -1)
 }
