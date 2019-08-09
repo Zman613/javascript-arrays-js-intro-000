cea6f7a59c448f3c50b5c4f50d8af5826a90ea1c
Chaim Muskal <monthlyschnitzel@gmail.com>
2019-08-09 23:03:12 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -18,7 +18,7 @@ function accessElementInArray(array, index){
   return [array[index]]
 }
 function destructivelyRemoveElementFromBeginningOfArray(array){
-  array.shift()
+  array.push()
   return array
 }
 function removeElementFromBeginningOfArray(array){
