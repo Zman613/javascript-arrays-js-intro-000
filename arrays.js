0fbbcd9e19ff8bbbb424dfda01d7d151222ce438
Chaim Muskal <monthlyschnitzel@gmail.com>
2019-08-09 20:16:53 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -19,3 +19,6 @@ function destructivelyRemoveElementFromBeginningOfArray(array){
   array.shift()
   return[array]
 }
+function removeElementFromBeginningOfArray(array){
+  return [array.slice(1)]
+}
